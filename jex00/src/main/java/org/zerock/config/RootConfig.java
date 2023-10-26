606e6f8bf63735eec4d5f90a1b45e8a02c29bd82
KimTaeGyun <133829003+taebak2@users.noreply.github.com>
2023-10-26 15:53:44 +0900

    Add files via upload

---
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -0,0 +1,11 @@
+package org.zerock.config;
+
+import org.springframework.context.annotation.Configuration;
+// @Configuration 어노테이션을 사용하면 
+// Spring은 해당 클래스를 검사하고 Bean 정의를 이해하며, 
+// 이를 사용하여 애플리케이션 컨텍스트를 설정
+
+@Configuration
+public class RootConfig {
+
+}
---
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,23 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {RootConfig.class};
+		// Spring 애플리케이션 컨텍스트가 초기화될 때 RootConfig 클래스의 구성이 적용
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return null;
+	}
+
+}
