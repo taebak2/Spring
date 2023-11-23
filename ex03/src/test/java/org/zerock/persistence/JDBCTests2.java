86fe7675fc121ee205f29f9c3902d85a0636888f
Kim-taegyun <xorbs3902@gmail.com>
2023-11-23 16:51:06 +0900

    Add files via upload

---
+++ ex03/src/test/java/org/zerock/persistence/JDBCTests2.java
@@ -0,0 +1,34 @@
+package org.zerock.persistence;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class JDBCTests2 {
+	static {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testConnection() {
+		String url = "jdbc:mysql://localhost:3306/member?useUnicode=true&characterEncoding=utf8&useSSL=false";
+		String id = "manager";
+		String pw = "1234";
+		
+		try (Connection con = DriverManager.getConnection(url,id,pw)) {
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+	}
+}
